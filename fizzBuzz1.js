d9fe80f39a92bb97e93c81e25bf9b660e97dc02b
Abdullah Shahzad <abdulashazad1@gmail.com>
2026-02-18 13:07:12 +0500

    fizzBuzzz verson 1.

---
+++ fizzBuzz1.js
@@ -0,0 +1,9 @@
+var number = prompt("Enter a number:")
+
+if (number%3 === 0 && number%5 === 0){
+    alert("FizzBuzz")
+} else if (number % 3 === 0 ){
+    alert("Fizz")
+} else if (number % 5 === 0 ){
+    alert("Buzz")
+} 
