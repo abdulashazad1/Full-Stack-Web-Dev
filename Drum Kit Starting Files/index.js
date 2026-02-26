cc18ec477a5ee352ef8dd303bf6c8000d0184ac3
Abdullah Shahzad <abdulashazad1@gmail.com>
2026-02-26 14:51:04 +0500

    sound 1 implemented

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,6 +1,11 @@
+var audio = new Audio('sounds/tom-1.mp3');
+
 for (var i =0; i<document.querySelectorAll(".drum").length; i++){
     document.querySelectorAll(".drum")[i].addEventListener('click', function(){
-        alert('I got clicked!');
+        audio.play();
     })
 }
+
+
+
  
