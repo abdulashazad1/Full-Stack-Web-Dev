278d04069cdaed1155fd06ee03bab5a0602c0532
Abdullah Shahzad <abdulashazad1@gmail.com>
2026-02-27 17:49:59 +0500

    added housekeeping function and other ufnctions that make housekeeping possible.

--- HouseKeeper.js
+++ HouseKeeper.js
@@ -3,8 +3,30 @@ function HouseKeeper(name, age, yearOfExperience, languages){
     this.age = age;
     this.yearOfExperience = yearOfExperience;
     this.languages = languages;
+
+    this.houseKeeping = function() {
+        alert("May I clean your room?")
+        entersRoom();
+        cleansRoom();
+        exitsRoom();
+    }
 }
+
+// these function have to be outside the object constructor function
+
+entersRoom = function (){
+        alert("*Housekeeping has entered the room*")
+    }
+
+cleansRoom = function (){
+        alert("I finished cleaning your room!")
+    }
+
+exitsRoom = function (){
+        alert("*Housekeeping has left the room*")
+    }
 
 var houseKeeper1 = new HouseKeeper("Zara", 22, 5, ["French", "English"]);
 console.log(houseKeeper1.name);
 console.log(houseKeeper1.languages);
+console.log(houseKeeper1.houseKeeping());
